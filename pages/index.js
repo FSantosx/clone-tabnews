b7fd91e5771115d25a3bf83f84f3790c0a0cf9f8
Fabio Santos <fabiojunior96@outlook.com>
2025-11-29 18:22:35 +0000

    Added new phrase `index.js`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 function Home() {
-  return <h1>Em construção</h1>;
+  return (
+    <>
+      <h1>O Jogo</h1>
+      <span>Perdi</span>
+      <br/>
+      <a href="https://pt.wikipedia.org/wiki/O_Jogo_(jogo_mental)">Explicação aqui</a>
+    </>
+  );
 }
 
 export default Home;
