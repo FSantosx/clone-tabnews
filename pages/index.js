9ce06d4df844baf9700319d887a79c7e983fabca
Fabio Santos <fabiojunior96@outlook.com>
2025-12-02 14:13:19 +0000

    fix files with `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,10 @@ function Home() {
     <>
       <h1>O Jogo</h1>
       <span>Perdi</span>
-      <br/>
-      <a href="https://pt.wikipedia.org/wiki/O_Jogo_(jogo_mental)">Explicação aqui</a>
+      <br />
+      <a href="https://pt.wikipedia.org/wiki/O_Jogo_(jogo_mental)">
+        Explicação aqui
+      </a>
     </>
   );
 }
